11ccb1b0fe03618adee993b7d06c611033148fa0
Jordan Trask <jtrask@gmail.com>
2022-09-08 17:11:53 +0000

    Updated

--- betteruptime.sh
+++ betteruptime.sh
@@ -10,6 +10,7 @@ DEBUG="0"
 # -- Colors
 RED="\e[31m"
 GREEN="\e[32m"
+CYAN="\e[36m"
 BLUEBG="\e[44m"
 YELLOWBG="\e[43m"
 GREENBG="\e[42m"
@@ -20,10 +21,27 @@ ECOL="\e[0m"
 # -- Help
 # -------
 USAGE=\
-"$0 <command>
+"$0 [-a <apikey>|-d] <command>
 
-Betteruptime API Credentials should be placed in \$HOME/.cloudflare
-  BU_KEY=\"\"
+Commands:
+	test			 - Test Better Uptime API key.
+	list             - List Monitors
+	create           - Create Monitors
+
+Options:
+    -a               - Better Uptime apikey team (Optional)
+    -d               - Debug mode (Optional)
+
+API Key:
+    The Better Uptime API Credentials should be placed in \$HOME/.cloudflare
+    Since there is a separate API key for teams, you can set a default and set
+    a team API key. The format as follows.
+
+	Default API Key:  BU_KEY=\"\"
+    Team API Key:     TEAM_BU_KEY=\"\"
+    
+    Replace TEAM with your placeholder and pass \"-a TEAM\" option. If -a isn't
+    set then the default BU_KEY is used.
 
 Version: $VERSION
 "
@@ -55,7 +73,7 @@ _separator () {
 
 _debug () {
     if [ -f $SCRIPT_DIR/.debug ]; then
-        echo "DEBUG: $@"
+        echo -e "${CYAN}** DEBUG: $@${ECOL}"
     fi
 }
 
@@ -73,13 +91,15 @@ usage () {
 betteruptime-api() {
 	local $API_PATH	
 	API_PATH=$1
+	_debug "\$API_PATH: $API_PATH"
 
+#    if [[ $DEBUG == "1" ]];then set +x;fi
 	CURL_OUTPUT=$(curl -s --request GET \
-		 --url https://betteruptime.com/${API_PATH} \
-  	     --header 'Authorization: Bearer "'${BU-KEY}'"')
+		 --url "https://betteruptime.com${API_PATH}" \
+		 --header 'Authorization: Bearer '"${BU_KEY}"'')
+#    if [[ $DEBUG == "1" ]];then set -x;fi
   	_debug "$CURL_OUTPUT" 	     
-	CURL_OUTPUT_JQ=$(echo $CURL_OUTPUT | jq -r)
-    return $CURL_OUTPUT_JQ  	
+	echo ${CURL_OUTPUT} | jq -r 
 }
 
 # -- betteruptime-api-creds
@@ -95,20 +115,46 @@ betteruptime-api-creds() {
 }
 
 # -- betteruptime-api-test
-betteruptime-api-test() {
-	betteruptime-api
+betteruptime-api-test() {	
+	betteruptime-api /api/v2/monitors
 }
 
 # ------------
 # -- Main loop
 # ------------
 
+# -- check better uptime credentials
 betteruptime-api-creds
-betteruptime-api-test
 
-
+# -- check if parameters are set
+_debug "PARAMS: $@"
 if [[ -z $1 ]];then
 	usage
-else
-	echo "Nothing here yet"
+	exit 1
 fi
+
+# -- options
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a|--apikey)
+    API_KEY_TEAM="$2"
+    _debug "\$API_KEY_TEAM: $API_KEY_TEAM"
+    shift # past argument
+    shift # past value
+    ;;
+    -d|--debug)
+    DEBUG="1"
+    _debug "\$DEBUG: $DEBUG"
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
